4c7973d9600a4817b01b5f0887a1f859e185e79d
lsh-NJ <b24041413@njupt.edu.cn>
2025-01-15 22:34:04 +0800

    测试提交

--- .vscode/Cpp/ListNode.cpp
+++ .vscode/Cpp/ListNode.cpp
@@ -3,17 +3,10 @@
 */
 class MyLinkedList{
 public:
-    struct LinkedNode {
-        int val;
-        LinkedNode* next;
-        // 构造函数
-        LinkedNode(int val):val(val), next(nullptr){}
-    }
-
     // 初始化链表
     MyLinkedList() {
         // 定义一个虚拟头节点
-        _dummyHead = new LinkedNode(0);
+        _dummyHead = new ListNode(0);
         _size = 0;
     }
 
@@ -23,7 +16,7 @@ public:
         if(index < 0 || index > (_size - 1)) {
             return -1;
         }
-        LinkedNode *cur = _dummyHead->next;
+        ListNode *cur = _dummyHead->next;
         while(index) {
             cur = cur->next;
             index--;
@@ -34,7 +27,7 @@ public:
     // 插入头节点
     void addAtHead(int val) {
         // 为新节点开辟空间
-        LinkedNode* newNode = new LinkedNode(val);
+        ListNode* newNode = new ListNode(val);
         newNode->next = _dummyHead->next;
         _dummyHead->next = newNode;
         _size++;
@@ -42,8 +35,8 @@ public:
 
     // 插入尾节点
     void addAtTail(int val) {
-        LinkedNode* newNode = new LinkedNode(val);
-        LinkedNode* cur = _dummyHead->next;
+        ListNode* newNode = new ListNode(val);
+        ListNode* cur = _dummyHead;
         while(cur->next) {
             cur->next = cur->next->next;
         }
@@ -54,32 +47,37 @@ public:
 
     // 在链表第index个节点前面插入一个节点
     void addAtIndex(int index, int val) {
-        LinkedNode* newNode = new LinkedNode(val);
-        LinkedNode* cur = _dummyHead->next;
-        /*需要对index进行讨论：
-        1.<=0就是头插
-        2.==size就是尾插
-        3.>size返回空*/ 
-        if(index <= 0) {
-            newNode->next = _dummyHead->next
-        } else if(index == _size) {
-            while(cur->next) {
-                cur->next = cur->next->next;
-            }
-            cur->next = newNode;
-        } else if(index > _size) {
-            return ;
-        } else {
-            while(index--){
-                // 注意这里是插在前面
-                cur->next = cur->next->next;
-            }
-            newNode->next = cur->next;
-            cur->next = newNode;
+        if(index > _size) return;
+        if(index < 0) index = 0;
+        ListNode* newNode = new ListNode(val);
+        ListNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
         }
+        newNode->next = cur->next;
+        cur->next = newNode;
         _size++;
     }
+
+    // 删除第index个节点,>=size返回空
+    void deleteAtIndex(int index) {
+        if(index >= _size) {
+            return;
+        }
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        // 不记录要删除的点就会使点的地址消失
+        ListNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        // tmp之后会变成指向随机的野指针，所以要使他成为空指针
+        tmp = nullptr;
+        _size--;
+    }
+
 private:
     int _size;
-    LinkedNode* _dummyHead;
+    ListNode* _dummyHead;
 }

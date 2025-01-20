1a27296604e52084dcb6b24cf4ed7eb9bade0726
lsh-NJ <b24041413@njupt.edu.cn>
2025-01-20 21:14:23 +0800

    测试提交

---
+++ Cpp/344As.cpp
@@ -0,0 +1,9 @@
+#define swap(a, b) {a ^= b; b ^= a; a ^= b;}
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size() / 2; i++) {
+            swap(s[i], s[s.size() - i - 1]);
+        }
+    }
+};
---
+++ Cpp/VectorL.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<char> arr = {'a','b'};
+    cout << arr.size();
+    return 0;
+}
